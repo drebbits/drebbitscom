16568740e925c2f95143f5f7043a872ab12d9f72
Dreb Bits <drebbitanghol@gmail.com>
2016-02-29 11:13:48 +0800

    Show excerpt only for standard posts

--- archive.php
+++ archive.php
@@ -25,12 +25,16 @@ get_header(); ?>
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content-excerpt' );
+				if ( ! get_post_format() ) :
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content-excerpt' );
+				else :
+					get_template_part( 'template-parts/content' );
+				endif;
 
 			endwhile; ?>
 
